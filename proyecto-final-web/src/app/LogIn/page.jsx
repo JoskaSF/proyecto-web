1146da3f673f3605336e2cf59a46585ed59df3a4
Ulises-19 <reyesbod@gmail.com>
2024-12-05 15:52:44 -0800

    Agrege la funcionalidad del Log In

--- proyecto-final-web/src/app/LogIn/page.jsx
+++ proyecto-final-web/src/app/LogIn/page.jsx
@@ -1,6 +1,42 @@
+'use client'
 import React from 'react';
+import { useState } from 'react';
+import { auth } from '@/firebase/firebaseConfig';
+import { signInWithEmailAndPassword } from 'firebase/auth';
 
-function LoginPage() {
+const LoginPage = () => {
+
+  const [correo, setCorreo] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    
+    // Validar que los campos no se envien vacios
+    if (!correo || !password) {
+      setError("Ingresar todos los campos!");
+      return;
+    }
+
+    try {
+      const credencialUsuario = await signInWithEmailAndPassword(auth, correo, password);
+      const usuario = credencialUsuario.user;
+
+      // Si se verifica correctamente los datos
+      console.log(`El usuario ${usuario} a ingresado con exito!`);
+
+      // Limpiar los campos despues de ingresar
+      setCorreo('');
+      setPassword('');
+      setError('');
+    } catch (e) {
+      // Si no se pudo acceder
+      setError('Error al intentar ingresar', e);
+    }
+    setError('');
+  }
+
   return (
     <div
       className="min-h-screen bg-cover bg-center flex justify-center items-center"
@@ -9,28 +45,35 @@ function LoginPage() {
       <div className="bg-gray-200 bg-opacity-100 p-8 rounded-lg shadow-lg max-w-sm w-full">
         <h2 className="text-3xl font-bold text-center text-green-500 mb-6">Inicio de sesión</h2>
         
-        <form>
+        <form className='' onSubmit={handleSubmit}>
           <div className="mb-4">
-            <label htmlFor="username" className="block text-sm font-medium text-gray-700">Nombre de usuario</label>
+            <label htmlFor="username" className="block text-sm font-medium text-gray-700">Correo:</label>
             <input
               type="text"
               id="username"
+              value={correo}
               name="username"
               className="mt-1 p-2 w-full border border-gray-300 rounded-md"
-              placeholder="Ingresa tu nombre de usuario"
+              placeholder="Ingresa tu correo"
+              onChange={(e) => setCorreo(e.target.value)}
             />
           </div>
 
           <div className="mb-6">
-            <label htmlFor="password" className="block text-sm font-medium text-gray-700">Contraseña</label>
+            <label htmlFor="password" className="block text-sm font-medium text-gray-700">Contraseña:</label>
             <input
               type="password"
               id="password"
+              value={password}
               name="password"
               className="mt-1 p-2 w-full border border-gray-300 rounded-md"
               placeholder="Ingresa tu contraseña"
+              onChange={(e) => setPassword(e.target.value)}
             />
           </div>
+          
+          {/* Mostrar un error si no se llenaron los campos */}
+          {error && <p style={{ color: 'red' }}>{error}</p>}
 
           <button
             type="submit" formMethod='POST'
