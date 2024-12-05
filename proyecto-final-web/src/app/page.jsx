efb5b070923c7d32db1981affd34788409a67d48
JoskaSF <al22760905@ite.edu.mx>
2024-12-05 12:48:22 -0800

    Comienzo de trabajo en Main

--- proyecto-final-web/src/app/page.jsx
+++ proyecto-final-web/src/app/page.jsx
@@ -1,17 +1,13 @@
 import React from 'react'
 import Link from 'next/link'
+import { AiFillFire } from "react-icons/ai"
 
 const MainPage = () => {
   return (
-    <div className='relative max-w-[100vw] max-h-[100vh]'>
+    <div className='relative max-w-[100vw] max-h-[100vh] overflow-hidden'>
       <div className='sticky top-0 min-h-[12vh] w-screen bg-[#41B149] shadow-xl z-50 flex items-center justify-end'>
         <Link href={'/LogIn'} className='mr-[5%]'>Iniciar Sesion</Link>
       </div>
-
-      <img 
-        src="/images/background.png" 
-        className="absolute top-[-20vh] left-[50%] w-[90%] rotate-[-20deg] z-0"
-      />
     </div>
   )
 }
