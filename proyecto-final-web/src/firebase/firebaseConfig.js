03a8d7ed9f67f89b80b75c8a38b3e6b1679ce992
Sugey Ramirez <al22760593@ite.edu.mx>
2024-12-04 23:17:02 -0800

    SDK firebase

--- proyecto-final-web/src/firebase/firebaseConfig.js
+++ proyecto-final-web/src/firebase/firebaseConfig.js
@@ -1,7 +1,17 @@
-import React from 'react'
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-export const firebaseConfig = () => {
-  return (
-    <div>firebaseConfig</div>
-  )
-}
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDUiyDC7V8kDSfiI3HYOAvpq54JWOMo1tM",
+  authDomain: "proyectowebdb-6bedd.firebaseapp.com",
+  projectId: "proyectowebdb-6bedd",
+  storageBucket: "proyectowebdb-6bedd.firebasestorage.app",
+  messagingSenderId: "749872551857",
+  appId: "1:749872551857:web:e1230da384b9c7a8274f90"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
