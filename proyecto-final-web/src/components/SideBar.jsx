3fb6dd48b72c27bb6ae11d2f9d66e4cb97831b60
Ulises-19 <reyesbod@gmail.com>
2024-12-05 23:10:05 -0800

    Implementar el cerrar sesion usando el boton en el SideBar

--- proyecto-final-web/src/components/SideBar.jsx
+++ proyecto-final-web/src/components/SideBar.jsx
@@ -1,7 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Link from 'next/link'
+import { signOut } from 'firebase/auth';
+import { auth } from '@/firebase/firebaseConfig';
+import { useRouter } from 'next/navigation';
 
 const SideBar = () => {
+    const [cliente, setCliente] = useState(false);
+    // Vamos a redirigir al usuario
+    const router = useRouter();
+
+    useEffect(() => {
+        setCliente(true);
+    }, []);
+
+    const handleLogOut = async () => {
+        // Tratar de cerrar sesion
+        try {
+            // cerrar la autentificacion
+            await signOut(auth);
+            // Desplegar en consola
+            console.log("Se cerró con exito la sesión");
+            // redireccion al LogIn
+            if (cliente) {
+                router.push('/LogIn'); 
+            }
+        } catch (e) {
+            console.log("Error: no se pudo cerrar sesión");
+        }
+    };
+
+    if (!cliente) {
+        return null;
+    }
+
     return (
         <aside className='bg-red-300 h-screen w-[20vw] flex flex-col p-[3%]'>
             <Link href={'/Home'}>Home</Link>
@@ -12,7 +43,7 @@ const SideBar = () => {
                 <option value="">Opcion 1</option>
                 <option value="">Opcion 2</option>
             </select>
-            <Link href={'/'}>Cerrar Sesion</Link>
+            <button onClick={handleLogOut}>Cerrar Sesion</button>
         </aside>
     )
 }   
