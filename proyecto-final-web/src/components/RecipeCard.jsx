d997d118545fcdcad579c2d5ff865bb9554b9d5d
Sugey Ramirez <al22760593@ite.edu.mx>
2024-12-05 23:27:06 -0800

    Funcion de boton ver mas

--- proyecto-final-web/src/components/RecipeCard.jsx
+++ proyecto-final-web/src/components/RecipeCard.jsx
@@ -11,21 +11,21 @@ const RecipeCard = ({name, ingredients, image, instructions}) => {
         {/* Nombre de la receta */}
         <div className='w-auto flex text-center text-white justify-center text-2xl' >{name}</div>
         <div className='text-white font-semibold my-1'>Ingredientes:</div>
-        <div className="bg-white rounded-sm flex flex-wrap">
+        <div className="bg-white rounded-sm flex flex-wrap text-justify">
             <p>{ingredients.join(', ')}</p>
         </div>  
-        <div className=''>
+        <div className='flex justify-items-center'>
             <img src={image} alt={name} style={{width:'20vw', height:'27vh'}} />
         </div>  
         {showInstructions && (
             <>
             <div className='text-white font-semibold my-1' >Instrucciones:</div>
-            <p className='bg-white rounded-sm text-justify' >
+            <p style={{whiteSpace: 'pre-line'}} className='bg-white rounded-sm text-justify' >
                 {instructions}</p>
+            <button className='m-2 w-20 h-10 bg-orange-500'>Hecha</button>
             </>
         )}        
         
-        <button className='m-2 w-20 h-10 bg-orange-500'>Hecha</button>
         <button onClick={toggleInstructions} className='m-2 w-20 h-10 bg-orange-500'>
             {showInstructions ? 'Ocultar' : 'Ver más'}
         </button>
