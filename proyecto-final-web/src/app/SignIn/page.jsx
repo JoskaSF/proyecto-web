0d5a5973f0f665e86f07f7043c8bff05524d8e1d
JoskaSF <al22760905@ite.edu.mx>
2024-12-06 07:06:02 -0800

    Iconos extras Sign in

--- proyecto-final-web/src/app/SignIn/page.jsx
+++ proyecto-final-web/src/app/SignIn/page.jsx
@@ -1,5 +1,6 @@
 'use client'
-import React from 'react' 
+import React from 'react'
+import Link from 'next/link'
 import { db } from '@/firebase/firebaseConfig';
 import { addDoc, collection } from 'firebase/firestore'
 import { useState } from 'react';
@@ -46,7 +47,10 @@ const SignIn = () => {
     return (
         <div className='w-screen relative'>
             <img src="/images/signBg.webp" alt="Kitchen" className='absolute inset-0 h-screen w-screen object-cover'/>
-            <div className='absolute w-[40vw] max-h-screen min-h-screen right-0 p-5 sign_cristal flex flex-col overflow-hidden'>
+            <div className='absolute sign_cristal rounded-r-[30px] w-32 h-20 top-16 left-0 z-50'>
+                <Link href={'/'} className='h-full flex justify-end items-center'><img src="/images/Logo.png" alt="recetitas" className='rounded-full h-[70%] mr-5'/></Link>
+            </div>
+            <div className='absolute w-[40vw] max-h-screen min-h-screen right-0 p-5 sign_cristal rounded-l-[30px] flex flex-col overflow-hidden'>
                 <h1 className='text-white text-[3rem] font-mono flex justify-center items-center'>Sign In<MdFoodBank className='ml-4 text-[4rem]'/></h1>
                 <form className='' onSubmit={enviar}>
                     <div className='flex flex-col'>
@@ -79,13 +83,14 @@ const SignIn = () => {
                 
                     {/* Mostrar un error si no se llenaron los campos */}
                     {error && <p style={{ color: 'red' }}>{error}</p>}
-                    <div className='flex justify-end'>
-                        <button type='submit' className='text-black bg-white p-2 rounded-xl my-5 mr-10'>Crear Usuario</button>
+                    <div className='flex flex-col items-end'>
+                        <Link href={'/Home'} className='text-gray-300 my-1 mr-10'>Ya tengo un cuenta.</Link>
+                        <button type='submit' className='text-black bg-white p-2 w-32 rounded-xl my-5 mr-10'>Crear Usuario</button>
                     </div>
                 </form>
                 <p className='text-white text-[1.5rem] font-mono flex justify-center'>Bienvenidos a Recetitas !!!</p>
                 <div className='w-full flex justify-center my-auto'>
-                    <div className='bg-red-400 w-[50%] aspect-square relative flex overflow-hidden'>
+                    <div className='bg-red-400 w-[45%] aspect-square relative flex overflow-hidden'>
                         <img src="/images/test1.png" alt="Prueba" className='h-full w-full object-cover carrusel'/>
                         <img src="/images/test2.png" alt="Prueba" className='h-full w-full object-cover carrusel'/>
                         <img src="/images/test3.png" alt="Prueba" className='h-full w-full object-cover carrusel'/>
