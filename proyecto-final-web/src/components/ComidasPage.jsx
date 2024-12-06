b2317c2f068f2ac3913d82bc599153c7afa75875
Sugey Ramirez <al22760593@ite.edu.mx>
2024-12-06 05:41:53 -0800

    Arregladote

--- proyecto-final-web/src/components/ComidasPage.jsx
+++ proyecto-final-web/src/components/ComidasPage.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+'use client'
+import React, {useEffect, useState} from 'react'
+import RecipeCard from '@/components/RecipeCard'
+import SideBar from '@/components/SideBar'
+import { GiFullPizza } from "react-icons/gi";
 
 const ComidasPage = () => {
-    const [meals, setMeals] =  useState([]); //Aqui se almacenan las meals, en un array
-    const [loading, setLoading] =  useState(true); 
-    const [categoria, setCategoria] = useState("");
+  const [meals, setMeals] =  useState([]); //Aqui se almacenan las meals, en un array
+  const [loading, setLoading] =  useState(true); 
+  const [categoria, setCategoria] = useState("");
+  const [options, setOptions] = useState([]);
 
-    useEffect(() => {
+  const handleChange = (event) => {
+    setCategoria(event.target.value);
+  };
+
+  useEffect(() => {
+    const fetchOptions = async () => {
+        try {
+            const response = await fetch('https://www.themealdb.com/api/json/v1/1/categories.php');
+            const data = await response.json();
+            setOptions(data.categories || []); // Maneja posibles resultados nulos
+        } catch (error) {
+            console.error('Error al cargar las opciones:', error);
+        }
+    };
+    fetchOptions();
+  }, []);
+
+  useEffect(() => {
     if (!categoria) return;
 
     const fetchMeals = async () => {
@@ -38,10 +60,23 @@ const ComidasPage = () => {
   },[categoria]);
   return (
     <div className='flex h-screen'>
-        <SideBar setCategoria={setCategoria}/>
-        <main className="flex-1 bg-gray-100 p-6 overflow-y-auto">
+      <SideBar/>
+      <main className="flex-1 p-6 overflow-y-auto">
+        <select onChange={handleChange} defaultValue="" className='bg-gray-200 rounded-2xl p-2'>
+            <option value="" disabled>
+                Selecciona una categoria
+            </option>
+            {options.map((option) => (
+                <option key={option.idCategory} value={option.strCategory}>
+                    {option.strCategory}
+                </option>
+            ))}
+        </select>
         {loading ?(
-          <p>Cargando recetas...</p> //JOSKA MODIFICA ESTE CARGANDO RECETAS
+          <div className='mt-[40vh] flex items-center justify-center gap-4 text-[2.5rem]'>
+            <GiFullPizza className='text-[#41B149] spin text-[6rem]'/>
+            <p>Esperando Categoria...</p>
+          </div> 
         ) : (
           meals.map((meal) => (
             <RecipeCard
@@ -52,10 +87,11 @@ const ComidasPage = () => {
                 instructions={meal.strInstructions}
               />
           ))
-        )};
+        )}
       </main>
-    </div>   
+    </div>
+
   );
-}
+};
 
 export default ComidasPage
