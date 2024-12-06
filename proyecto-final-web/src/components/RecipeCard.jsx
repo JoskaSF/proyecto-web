a44be87d7e002226235d6c427b5cf21279eff0c7
Sugey Ramirez <al22760593@ite.edu.mx>
2024-12-05 22:48:47 -0800

    Funcion boton ver mas

--- proyecto-final-web/src/components/RecipeCard.jsx
+++ proyecto-final-web/src/components/RecipeCard.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const RecipeCard = ({name, ingredients, image, instructions}) => {
+    const [showInstructions, setShowInstructions] = useState(false);
+
+    const toggleInstructions = () => {
+        setShowInstructions(!showInstructions); // Alterna el estado de visibilidad
+      };
   return (
-    <div className='w-4/12 h-auto rounded-xl display fljustify-center items-center bg-slate-500 p-5 m-10'>
+    <div className='w-auto h-auto rounded-xl display fljustify-center items-center bg-slate-500 p-5 m-10'>
         {/* Nombre de la receta */}
         <div className='w-auto flex text-center text-white justify-center text-2xl' >{name}</div>
         <div className='text-white font-semibold my-1'>Ingredientes:</div>
@@ -11,11 +16,19 @@ const RecipeCard = ({name, ingredients, image, instructions}) => {
         </div>  
         <div className=''>
             <img src={image} alt={name} style={{width:'20vw', height:'27vh'}} />
-        </div>          
-        <div className='text-white font-semibold my-1 hidden' >Instrucciones:</div>
-        <div className='bg-white rounded-sm text-justify hidden' >{instructions}</div>
+        </div>  
+        {showInstructions && (
+            <>
+            <div className='text-white font-semibold my-1' >Instrucciones:</div>
+            <p className='bg-white rounded-sm text-justify' >
+                {instructions}</p>
+            </>
+        )}        
+        
         <button className='m-2 w-20 h-10 bg-orange-500'>Hecha</button>
-        <button className='m-2 w-20 h-10 bg-orange-500'>Ver más</button>
+        <button onClick={toggleInstructions} className='m-2 w-20 h-10 bg-orange-500'>
+            {showInstructions ? 'Ocultar' : 'Ver más'}
+        </button>
 
     </div>  
 
